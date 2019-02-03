a098bb1075cd286005383359c521ac1ae70dcc64
myckhel <myckhel1@hotmail.com>
2019-02-03 16:47:27 +0100

    built game of fifteen

---
+++ build/precache-manifest.41ade2c9e58f42f4625b133187ac2ba8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fbfd6aeec2f63bff741d",
+    "url": "/static/js/main.fbfd6aee.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "fbfd6aeec2f63bff741d",
+    "url": "/static/css/main.bda6c24c.chunk.css"
+  },
+  {
+    "revision": "d7b4a6621255e53c2e48f6cb3acae1a9",
+    "url": "/index.html"
+  }
+];
