596ab357884b60c8d69faaec4bf71bc3ca7c455b
myckhel <myckhel1@hotmail.com>
2019-02-03 17:24:19 +0100

    adde github page for deployment

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41ade2c9e58f42f4625b133187ac2ba8.js"
+  "/react-game-of-fifteen/precache-manifest.ce72c27a304c3498d7aca479b9e108b2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-game-of-fifteen/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
