005130bc485f8c18c989f30bd0983e30558bdb9b
myckhel <myckhel1@hotmail.com>
2019-02-04 10:08:31 +0100

    Updates

---
+++ precache-manifest.ce72c27a304c3498d7aca479b9e108b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13ac67f691e2136899a3",
+    "url": "/react-game-of-fifteen/static/js/runtime~main.13ac67f6.js"
+  },
+  {
+    "revision": "3be795d05644384632fa",
+    "url": "/react-game-of-fifteen/static/js/main.3be795d0.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/react-game-of-fifteen/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "3be795d05644384632fa",
+    "url": "/react-game-of-fifteen/static/css/main.bda6c24c.chunk.css"
+  },
+  {
+    "revision": "ef1acdfbf99600010bded9e05d1a46b2",
+    "url": "/react-game-of-fifteen/index.html"
+  }
+];
