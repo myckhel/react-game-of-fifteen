203b231344fe4988ad03b47cdda0e10645df2da8
Hoffenheim Technologies <myckhel1@hotmail.com>
2019-02-04 12:29:29 +0100

    rebuilt project

--- build/precache-manifest.3c78d1b16bbb15c4c3184678e2feaee5.js
+++ build/precache-manifest.3c78d1b16bbb15c4c3184678e2feaee5.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-game-of-fifteen/static/js/runtime~main.13ac67f6.js"
   },
   {
-    "revision": "3be795d05644384632fa",
-    "url": "/react-game-of-fifteen/static/js/main.3be795d0.chunk.js"
+    "revision": "0504829198962574eb0c",
+    "url": "/react-game-of-fifteen/static/js/main.05048291.chunk.js"
   },
   {
     "revision": "f739feb7d61c48a46bed",
     "url": "/react-game-of-fifteen/static/js/1.f739feb7.chunk.js"
   },
   {
-    "revision": "3be795d05644384632fa",
-    "url": "/react-game-of-fifteen/static/css/main.bda6c24c.chunk.css"
+    "revision": "0504829198962574eb0c",
+    "url": "/react-game-of-fifteen/static/css/main.09002336.chunk.css"
   },
   {
-    "revision": "ef1acdfbf99600010bded9e05d1a46b2",
+    "revision": "8d0386059ed4c7a39e84c3579e395ca4",
     "url": "/react-game-of-fifteen/index.html"
   }
 ];
